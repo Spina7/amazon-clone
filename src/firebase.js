b57326282bf90f9585abfcafc96e34c66ed73daa
Spina <alonzopina7@gmail.com>
2020-10-25 17:40:39 -0600

    archivo firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBIaZeb09Ki0pXzSUdhJGyCleUTFrGdi6k",
+    authDomain: "clone-c1395.firebaseapp.com",
+    databaseURL: "https://clone-c1395.firebaseio.com",
+    projectId: "clone-c1395",
+    storageBucket: "clone-c1395.appspot.com",
+    messagingSenderId: "1090986835950",
+    appId: "1:1090986835950:web:df9ce3e5bfff0aca7598ff",
+    measurementId: "G-2EQ3KY6JVP"
+  };
