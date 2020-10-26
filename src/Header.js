9085900f1e97cda135a9a794c053711d9d0bbd6f
Spina <alonzopina7@gmail.com>
2020-10-25 18:07:38 -0600

    header nav

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,13 @@ import './Header.css'
 function Header() {
     return (
         <div className='header'>
-            
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
+            <div className="header__search">
+                <input className="hedaer__searchInput" type="text"/>
+            </div>
+            <div className="header__nav">
+                
+            </div>
         </div>
     )
 }
