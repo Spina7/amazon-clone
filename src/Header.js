b21b836fa37d2f76f21ab882fdb0deb715531d59
Spina <alonzopina7@gmail.com>
2020-10-26 00:16:35 -0600

    añadiendo el home component

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
     return (
         <div className='header'>
             <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
             <div className="header__search">
-                <input className="hedaer__searchInput" type="text"/>
+                <input className="header__searchInput" type="text"/>
+                <SearchIcon className="header__searchIcon"/>
             </div>
             <div className="header__nav">
                 <div className="header__option">
@@ -25,8 +28,9 @@ function Header() {
                     <span className='header__optionLineTwo'>Prime</span>
 
                 </div>
-                <div className="header__option">
-
+                <div className="header__optionBasket">
+                <ShoppingBasketIcon/>
+                <span className="header__optionLinetwo header__basketCount">0</span>
                 </div>
             </div>
         </div>
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Home.css";
+function Home() {
+    return (
+        <div class='home'>
+            <p>home component</p>
+        </div>
+    )
+}
+
+export default Home
