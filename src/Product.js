184dfebbdf6cf9aeb9a62b4a9a3bb0ecf590f0d1
Spina <alonzopina7@gmail.com>
2020-10-26 01:53:29 -0600

    productos añadidos con flexbox

---
+++ src/Product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Product.css";
+function Product({title, image, price, rating}) {
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating).fill().map((_,i) => (<p>⭐</p>))}
+                </div>
+                </div>
+                <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"/>
+        
+            
+            <button>Añadir al Carrito</button>
+        </div>
+    )
+}
+
+export default Product
