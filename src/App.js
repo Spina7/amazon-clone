098bff4c930aff3296a30274379bac57d1057432
Spina <alonzopina7@gmail.com>
2020-10-25 23:32:11 -0600

    icons-setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hola Mundo</h1>
+      
+     <Header />
+
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -9,7 +9,25 @@ function Header() {
                 <input className="hedaer__searchInput" type="text"/>
             </div>
             <div className="header__nav">
-                
+                <div className="header__option">
+                    <span className='header__optionLineOne'>Hola Invitado</span>
+                    <span className='header__optionLineTwo'>Iniciar Sesion</span>
+
+                </div>
+                <div className="header__option">
+                <span className='header__optionLineOne'>Devoluciones</span>
+                    <span className='header__optionLineTwo'>y Pedidos</span>
+
+
+                </div>
+                <div className="header__option">
+                <span className='header__optionLineOne'>Your</span>
+                    <span className='header__optionLineTwo'>Prime</span>
+
+                </div>
+                <div className="header__option">
+
+                </div>
             </div>
         </div>
     )
