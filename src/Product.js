bf24af3d4385b38d73b1e8748f463f0fe77165d2
Spina <alonzopina7@gmail.com>
2020-10-26 19:24:22 -0600

    reducer agregado

--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import "./Product.css";
-function Product({title, image, price, rating}) {
+import { useStateValue } from './StateProvider';
+function Product({id, title, image, price, rating}) {
+    const [{basket}, dispatch]= useStateValue();
+    
+    const addToBasket = () => {
+        //dispatch the item into the data layer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
     return (
         <div className="product">
             <div className="product__info">
@@ -16,7 +32,7 @@ function Product({title, image, price, rating}) {
                 <img src={image}/>
         
             
-            <button>Añadir al Carrito</button>
+            <button onClick={addToBasket}>Añadir al Carrito</button>
         </div>
     )
 }
